8cb9f31dfe5cfcb5ee1451bcde5687e2487dc3c1
Arnaud BRETON <abreton@mailjet.com>
2016-04-22 12:45:15 +0200

    First working version

---
+++ src/crawler.js
@@ -0,0 +1,112 @@
+import request from 'request'
+import debug from 'debug'
+import EventEmitter from 'events'
+import config from './config'
+const debugLogger = debug('mjml-gist-crawler')
+
+const readGists = (since) => {
+    debugLogger(`Gist loading...`)
+    debugLogger(`https://api.github.com/users/mjml-tryitlive/gists?access_token==${config.crawler.gist_token}&&since=${since.toISOString()}`)
+    const promise = new Promise((resolve, reject) => {
+      let gistContent = ''
+      request(
+        {
+          url: `https://api.github.com/users/mjml-tryitlive/gists?access_token=${config.crawler.gist_token}&since=${since.toISOString()}`,
+          method: 'GET',
+          headers: {
+            'User-Agent': 'mjml-gist-crawler'
+          }
+        },
+        (error, response, body) => {
+          if (error) {
+            debugLogger(`Gist loaded with error ${error}`)
+            reject()
+          } else {
+            resolve(JSON.parse(body))
+          }
+      })
+    })
+
+    return promise
+}
+
+const readGist = (gist) => {
+  debugLogger(`Gist loading ${gist.id}...`)
+  const promise = new Promise((resolve, reject) => {
+    request(
+      {
+        url: `https://api.github.com/gists/${gist.id}?access_token==${config.crawler.gist_token}&`,
+        method: 'GET',
+        headers: {
+          'User-Agent': 'mjml-gist-crawler'
+        }
+      },
+      (error, response, body) => {
+        if (error) {
+          debugLogger(`Gist ${gist.id} loaded with error ${error}`)
+          reject()
+        } else {
+          debugLogger(`Gist ${gist.id} loaded successfully`)
+          resolve(JSON.parse(body))
+        }
+    })
+  })
+
+  return promise
+}
+
+const sendMJMLEmail = (content) => {
+  console.log(`Sending ${content} over email`)
+  const promise = new Promise((resolve, reject) => {
+    request(
+      {
+        url: `${config.crawler.api_base_url}/render-send-email`,
+        method: 'POST',
+        body: JSON.stringify({
+          mjml: content,
+          recipients: [config.crawler.recipient]
+        }),
+        headers: {
+          'User-Agent': 'mjml-gist-crawler',
+          'Content-Type': 'application/json'
+        }
+      },
+      (error, response, body) => {
+        if (error) {
+          debugLogger(`Gist not sent`)
+          reject()
+        } else {
+          debugLogger(`Gist sent`)
+          resolve(body)
+        }
+    })
+  })
+
+  return promise
+}
+
+const crawl = (since) => {
+  const promise = new Promise((resolve, reject) => {
+    readGists(since)
+      .then(gists => Promise.all(gists))
+      .then(gists => Promise.all(gists.map(gist => readGist(gist))))
+      .then(gists => {
+        Promise.all(gists.map(gist => {
+          sendMJMLEmail(gist.files.tryItLive.content)
+        }))
+      })
+      .then(() => {
+        setTimeout(() => {
+          myEmitter.emit('crawl', new Date(since.getTime() + 10*60000))
+        }, 0.5*60000)
+      })
+  })
+
+  return promise
+}
+
+crawl(new Date(config.crawler.start_date))
+const myEmitter = new EventEmitter();
+myEmitter.on('crawl', (since) => {
+  crawl(since)
+})
---
+++ src/index.js
@@ -0,0 +1,84 @@
+import config from './config'
+import bodyParser from 'body-parser'
+import mjmlEngine from 'mjml'
+import express from 'express'
+
+const app = express()
+const Mailjet = require('node-mailjet').connect(config.mailjet.apikey, config.mailjet.apisecret)
+
+app.use(bodyParser.json())
+
+const renderMJML = (mjml) => {
+  return new Promise((resolve, reject) => {
+    if (!mjml) {
+      reject({"error": "No MJML provided"})
+      return
+    }
+    else {
+      const html = mjmlEngine.mjml2html(mjml)
+      resolve({"html": html})
+      return
+    }
+  })
+}
+
+app.post('/render', (req, res) => {
+  const mjml = req.body.mjml
+  const promise = renderMJML(mjml)
+
+  promise.then((html) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.json({"mjml": mjml, "html": html.html})
+  })
+  .catch((error) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.status(400)
+    res.json(error)
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+})
+
+app.post('/render-send-email', (req, res) => {
+  const mjml = req.body.mjml
+  const recipients = req.body.recipients
+
+  if (!recipients || recipients.length === 0) {
+    res.setHeader('Content-Type', 'application/json')
+    res.status(400)
+    res.json({"error": "No recipients provided"})
+  }
+
+  const promise = renderMJML(mjml)
+
+  promise.then((html) => {
+    const recipientsMailjet = recipients.map((recipient) => { return {"Email": recipient}})
+    const emailData = {
+      'FromEmail': config.mailjet.sender,
+      'FromName': 'MJML Render Bot',
+      'Subject': 'Your MJML rendered',
+      'HTML-part': html.html,
+      'Recipients': recipientsMailjet
+    }
+
+    console.log(emailData)
+    const sendEmail = Mailjet.post('send')
+    sendEmail
+    	.request(emailData)
+      .on('success', () => {
+        res.setHeader('Content-Type', 'application/json')
+        res.json({"mjml": mjml, "html": html.html, "recipients": recipients})
+      })
+      .on('error', (error, response) => {
+        res.setHeader('Content-Type', 'application/json')
+        res.json({"error": error})
+      })
+  })
+  .catch((error) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.status(400)
+    res.json(error)
+  })
+})
